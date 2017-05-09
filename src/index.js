e82508ded3233b2cc825949ab6fee3aea5590d05
Wesley Ramalho <wesley_raa@hotmail.com>
2017-05-09 16:25:08 -0300

    Material UI configured

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 import './index.css';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MaterialTestComponent from './MaterialTestComponent';
+ 
+const App = () => (
+  <MuiThemeProvider>
+    <MaterialTestComponent />
+  </MuiThemeProvider>
+);
 ReactDOM.render(
   <App />,
   document.getElementById('root')
